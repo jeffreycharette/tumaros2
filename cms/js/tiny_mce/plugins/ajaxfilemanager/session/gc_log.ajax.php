fd22e8c098835943d8a4444d35493c9cb9c6cb5e
Jeff Charette <jeffrey@wearecharette.com>
2013-03-07 09:36:00 -0500

    deleting archives

--- cms/js/tiny_mce/plugins/ajaxfilemanager/session/gc_log.ajax.php
+++
@@ -1,2 +0,0 @@
-<?php die(); ?>
-gc start at 22/Feb/2010 19:47:17
--- cms/templates/template_list.js
+++
@@ -1,9 +0,0 @@
-var tinyMCETemplateList = [
-	// Name, URL, Description
-	["Container Width 230px", "templates/b_w230.htm", "Container with width of 230px."],
-	["Container Width 350px", "templates/b_w350.htm", "Container with width of 350px."],
-	["Container Width 470px", "templates/b_w470.htm", "Container with width of 470px."],
-	["Container Width 591px", "templates/b_w591.htm", "Container with width of 591px."],
-	["Container Width 712px", "templates/b_w712.htm", "Container with width of 712px."],
-	["Empty Entry Query", "templates/query.htm", "Entry query place holder."]
-];
